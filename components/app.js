4f16bb93a838f7dc413ae8b4d29355e18ed05d36
Teodor Garzdin <teodorgarzedin@gmail.com>
2017-05-06 18:14:14 +0200

    Track user’s location and monitor for changes.

--- components/app.js
+++ components/app.js
@@ -5,9 +5,33 @@ import Initial from './initial';
 StatusBar.setBarStyle('light-content');
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: null
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ location: position });
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.position = navigator.geolocation.watchPosition((position) => {
+      this.setState({ location: position });
+    }
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.position);
+  }
+
   render() {
     return (
-      <Initial />
+      <Initial location={this.state.location} />
     )
   }
 };
